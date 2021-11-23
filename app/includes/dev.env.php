6cdcb67ba1bb90d0cf75564620b902b22de71dc3
Ludivine Jamin <85514161+LudiJa@users.noreply.github.com>
2021-11-23 16:45:20 +0100

    Delete dev.env.php

--- app/includes/dev.env.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("SERVER", 'localhost');
-define("DATABASE", "c1759204c_wf3_php_intermediaire_ludivine");
-define("USER", 'c1759204c');
-define("PASSWORD", "67UdybfUJKnSxVe");
-
-// information de la base de données
